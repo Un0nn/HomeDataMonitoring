661ebade6293ad2602452e37af7f5b100475592a
Un0nn <112654901+Un0nn@users.noreply.github.com>
2025-04-05 18:40:55 -0400

    Simplified print

--- Aranet4/Aranet4Metrics.py
+++ Aranet4/Aranet4Metrics.py
@@ -1,6 +1,6 @@
 import time
 from prometheus_client import start_http_server, Gauge, CollectorRegistry
-import Aranet4DataReader
+
 from aranet4 import Aranet4Scanner
 import asyncio
 import time
@@ -13,32 +13,8 @@ def on_scan(advertisement):
     if not advertisement.readings:
         return
 
-    print("=======================================")
-    print(f"  Name:             {advertisement.device.name}")
-    print(f"  Model:            {advertisement.readings.type.model}")
-    print(f"  Address:          {advertisement.device.address}")
-
-    if advertisement.manufacturer_data:
-        mf_data = advertisement.manufacturer_data
-        print(f"  Version:          {mf_data.version}")
-        print(f"  Integrations:     {mf_data.integrations}")
-        # print(f"  Disconnected:      {mf_data.disconnected}")
-        # print(f"  Calibration state: {mf_data.calibration_state.name}")
-        # print(f"  DFU Active:        {mf_data.dfu_active:}")
-
-    print(f"  RSSI:             {advertisement.rssi} dBm")
-
-    if advertisement.readings:
-        print("--------------------------------------")
-        print(f"  CO2:           {advertisement.readings.co2} pm")
-        print(f"  Temperature:   {advertisement.readings.temperature:.01f} \u00b0C")
-        print(f"  Humidity:      {advertisement.readings.humidity} %")
-        print(f"  Pressure:      {advertisement.readings.pressure:.01f} hPa")
-        print(f"  Battery:       {advertisement.readings.battery} %")
-        print(f"  Status disp.:  {advertisement.readings.status.name}")
-        print(f"  Ago:           {advertisement.readings.ago} s")
-        g.set(advertisement.readings.co2)  # Set Prometheus data
-    print()
+    g.set(advertisement.readings.co2)  # Set Prometheus data
+    print(advertisement.readings.toString())
     time.sleep(SCRAPE_DELAY)
 
 
